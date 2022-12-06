e7ef689b19ef2f3da999be75fb3ec0ceb37da55f
youliang <tan_you_liang@hotmail.com>
2022-12-05 23:12:20 -0500

    update docs and missing script
    Signed-off-by: youliang <tan_you_liang@hotmail.com>

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # uav-landing-nlp
 Guided UAV Landing with Natural Language Instructions
 
+![archi](model_pipeline.png)
+
 There are two ML components here
- - CLIP (`cd clip` and check out docs there)
- - DeepLabv3 Semantic seg (checkout the jupyter notebook)
+ - CLIP (Contrastive Language-Image Pre-Training)
+   - check out the docs in the `clip` directory
+   - Training: `python3 train.py` (with transfer learning of pretrained weights)
+   - Zero-shot demonstration with cars: `clip_zero_shot_tests.ipynb`
+ - Aerial image Semantic seg 
+   - DeepLabV3+ model yields the best result
+   - checkout the jupyter notebook: `aerial-semantic-segmentationl.ipynb`
+ - Full NLP uav-landing pipeline:
+   - notebook: `full_nlp_landing_pipeline.ipynb`
+   - includes the "safety weighted landing selector"
 
 Dataset:
  - `aerial-sematic-segmentation-dataset`
    - Use for  `segmentic Segmentation`
    - https://www.kaggle.com/datasets/bulentsiyah/semantic-drone-dataset
  - `semantic_drone_dataset_processed`:
-   - Use for `CLIP`
+   - Use for `CLIP` model training, images are cropped and generated from the above "semantic-drone-dataset"
    - [link](https://drive.google.com/drive/folders/1CwQwrTuoVC0FptFpd1qd0Mx1VR9EuHyv)
    - Consist of redcross on aerial image to describe spatial info
+   - Added [COCO dataset](https://cocodataset.org/#download) to augment the dataset
---
+++ clip/utils.py
@@ -0,0 +1,37 @@
+import torch
+
+def tokenize(texts, tokenizer, context_length=77):
+    if isinstance(texts, str):
+        texts = [texts]
+
+    sot_token = tokenizer.encoder["<|startoftext|>"]
+    eot_token = tokenizer.encoder["<|endoftext|>"]
+    all_tokens = [[sot_token] + tokenizer.encode(text) + [eot_token] for text in texts]
+    result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
+
+    for i, tokens in enumerate(all_tokens):
+        if len(tokens) > context_length:
+            raise RuntimeError(f"Input {texts[i]} is too long for context length {context_length}")
+        result[i, :len(tokens)] = torch.tensor(tokens)
+
+    return result
+
+def zeroshot_classifier(model, classnames, templates, tokenizer, device):
+    '''
+    Creates texts for each class using templates and extracts their text embeddings.
+    '''
+    print("Getting text features from classnames")
+    with torch.no_grad():
+        zeroshot_weights = []
+        for classname in classnames:
+            texts = [template.format(' '.join(classname.split('_'))) for template in templates] #format with class
+            print("class texts :")
+            print(texts)
+            texts = tokenize(texts, tokenizer).to(device) #tokenize
+            class_embeddings = model.encode_text(texts) #embed with text encoder
+            class_embeddings /= class_embeddings.norm(dim=-1, keepdim=True)
+            class_embedding = class_embeddings.mean(dim=0)
+            class_embedding /= class_embedding.norm()
+            zeroshot_weights.append(class_embedding)
+        zeroshot_weights = torch.stack(zeroshot_weights, dim=1).to(device)
+    return zeroshot_weights
